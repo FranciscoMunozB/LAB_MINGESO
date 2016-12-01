264dde8e3540a889e8db4129c5468dab4d2aa632
fmunoz94 <francisco.munoz.b@usach.cl>
2016-12-01 17:08:39 -0300

    Primer commit.

---
+++ src/java/facade/ProductoFacade.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade;
+
+import model.Productos;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Joseph
+ */
+@Stateless
+public class ProductoFacade implements ProductoLocal {
+    @PersistenceContext(unitName = "MingesoCIPU")
+    private EntityManager em;
+
+    @Override
+    public void addProducto(Productos producto) {
+        em.persist(producto);
+    }
+
+    @Override
+    public void editProducto(Productos producto) {
+        em.merge(producto);
+    }
+
+    @Override
+    public void deleteProducto(int productoId) {
+        em.remove(getProducto(productoId));
+    }
+
+    @Override
+    public Productos getProducto(int productoId) {
+        return em.find(Productos.class, productoId);
+    }
+
+    @Override
+    public List<Productos> getAllProductos() {
+        return em.createNamedQuery("Productos.findAll").getResultList();
+    }
+
+}
---
+++ src/java/facade/ProductoLocal.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade;
+
+import model.Productos;
+import java.util.List;
+import javax.ejb.Local;
+/**
+ *
+ * @author Buging
+ */
+public interface ProductoLocal {
+
+    void addProducto(Productos producto);
+
+    void editProducto(Productos producto);
+
+    void deleteProducto(int productoId);
+
+    Productos getProducto(int productoId);
+
+    List<Productos> getAllProductos();
+    
+}
