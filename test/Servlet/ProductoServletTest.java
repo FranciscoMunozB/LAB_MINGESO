fb0a605fa00d163ceef5d636bcff6fcbe56b7b76
fmunoz94 <francisco.munoz.b@usach.cl>
2016-12-06 15:03:42 -0300

    commit test 1

---
+++ test/Servlet/ProductoServletTest.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author francisco
+ */
+public class ProductoServletTest {
+    
+    public ProductoServletTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of processRequest method, of class ProductoServlet.
+     */
+    @Test
+    public void testProcessRequest() throws Exception {
+        System.out.println("processRequest");
+        HttpServletRequest request = null;
+        HttpServletResponse response = null;
+        ProductoServlet instance = new ProductoServlet();
+        instance.processRequest(request, response);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of doGet method, of class ProductoServlet.
+     */
+    @Test
+    public void testDoGet() throws Exception {
+        System.out.println("doGet");
+        HttpServletRequest request = null;
+        HttpServletResponse response = null;
+        ProductoServlet instance = new ProductoServlet();
+        instance.doGet(request, response);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of doPost method, of class ProductoServlet.
+     */
+    @Test
+    public void testDoPost() throws Exception {
+        System.out.println("doPost");
+        HttpServletRequest request = null;
+        HttpServletResponse response = null;
+        ProductoServlet instance = new ProductoServlet();
+        instance.doPost(request, response);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getServletInfo method, of class ProductoServlet.
+     */
+    @Test
+    public void testGetServletInfo() {
+        System.out.println("getServletInfo");
+        ProductoServlet instance = new ProductoServlet();
+        String expResult = "";
+        String result = instance.getServletInfo();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
